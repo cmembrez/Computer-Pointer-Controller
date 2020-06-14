f2c610f313d06a88ddfa92e584a03f866d91d143
cmembrez <cedric.membrez@gmail.com>
2020-06-14 12:22:07 +0200

    Simple script for testing.

---
+++ test_simple.sh
@@ -0,0 +1,2 @@
+python3 ./src/main_computer_pointer_controller.py -itype 'video' -ipath './bin/demo.mp4' \
+                              -facialPrecision 'FP16' -headPrecision 'FP16' -gazePrecision 'FP16'
